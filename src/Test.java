6161574581a82a5c93c73c26161710a9a2c852f4
hirenitalia1411 <147237187+hirenitalia1411@users.noreply.github.com>
2024-01-27 15:11:41 +0000

    update in the test

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is Hiren");
+        System.out.println("My name is Hiren, how are you?");
     }
 }
